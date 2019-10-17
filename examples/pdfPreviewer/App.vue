7b24e92a2cc6f030a5a8c736340fc00816e9c66a
xinyanhui <xinyanhui@baidu.com>
2019-10-17 17:10:30 +0800

    提交readme

--- examples/pdfPreviewer/App.vue
+++ examples/pdfPreviewer/App.vue
@@ -32,7 +32,8 @@ export default {
         return {
             showLimitTip: false,
             isShowViewer: false,
-            url: 'https://qdcu02.baidupcs.com/file/e47222aeffec55187b691088ac207308?bkt=en-2a4ba40c42c88fab2e5255fcb74f1cf130671960734b2bfe094f49ad83845d223eeed717fac22a3e0e9bf5590699d7d849c37042b583ef6e556ef015706354d3&fid=3275707650-14482137-758931884500561&time=1571296778&sign=FDTAXGERQBHSKfW-DCb740ccc5511e5e8fedcff06b081203-pkT3dJX79AHS96kNnqzu%2FQdXtFo%3D&to=66&size=611769&sta_dx=611769&sta_cs=3&sta_ft=pdf&sta_ct=4&sta_mt=4&fm2=MH%2CYangquan%2CAnywhere%2C%2CNone%2Cany&ctime=1568728426&mtime=1568728426&resv0=cdnback&resv1=0&resv2=&resv3=&resv4=&vuk=3275707650&iv=2&htype=&randtype=&newver=1&newfm=1&secfm=1&flow_ver=3&pkey=en-5bfb1fd8818e10cff7ba474437f67ad1279a8416b705ff7982726dfa093ddb7a9f3aca9da4503d4905e18451569dc68fe0165bf8e8b5f505305a5e1275657320&expires=8h&rt=pr&r=730973330&mlogid=2594304664&vbdid=-&fin=1568728420-2086b0784449a3b304bcf61807bddea8_split_watermark.pdf&fn=1568728420-2086b0784449a3b304bcf61807bddea8_split_watermark.pdf&err_ver=1.0&check_blue=1&rtype=1&dp-logid=6717537693798020448&dp-callid=0.1.1&hps=1&tsl=0&csl=0&csign=nxjNh6UyYB5VasDoiqsxlpvduTw%3D&so=0&ut=1&uter=4&serv=0&uc=1022049119&ti=34c17094e7d5a0c9c362d21c3a87e8c6e5329518ab5402e4&by=themis'
+            // 随意输入一个pdf预览地址
+            url: ''
         }
     },
     methods: {
